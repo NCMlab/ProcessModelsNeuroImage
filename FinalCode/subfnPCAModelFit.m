d4b26b9b5851b2c3e54aca784720a5eea0017f42
Jason Steffener <js2746@columbia.edu>
2012-11-27 14:42:37 -0500

    added more notes about the steps to do

---
+++ FinalCode/subfnPCAModelFit.m
@@ -0,0 +1,46 @@
+function subfnPCAModelFit
+
+
+% for i = 1:Nsub
+%   Leave one out matrix=(Nsub-x-Ncombinations_of_PCs)
+%   Apply PCA
+%   for j = 1:all combinations of PCs
+%       fit the regression (linear/iteratively)
+%       predict left out subject
+%       find squared error (one number) 
+%   end
+% end
+% sum over all subjects
+% find the comboination of PCs that has the lowest error
+% 
+% for i = 1:Nboot
+%   resample
+%   Apply PCA
+%   pick combo of PCs as calculated in the leave one out procedure
+%   create the combo Image 
+%   save this image to the bootstrap (HUGE) resample array
+%           matrix=(Nboot-x-Nvoxels)
+%   fit the regression (linear/iteratively)
+%   for k = 1:Nprobe
+%       adjust the moderator term
+%       fit the regression (linear/iteratively)
+%       save parameter estimates
+%   end
+%   save parameter estimates
+% end
+% 
+% calculate the Z-map combo image using the bootstrap standard error
+% calculate the confidence intervals for each voxel of the combo image
+% calculate the confidence intervals for each regression parameter and the
+% conditional indirect effect
+% save to file:
+%   parameter maps
+%   conditional indirect effect maps at the probe values
+%   confidence interval maps for the parameter maps
+%   p-value maps for the parameter maps
+%   Z-map for the combo image
+%   combo image confidence intervals
+%   combo image p-value maps
+
+
+
