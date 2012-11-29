c1ee5b32f5c2fb08d25917e881a03b9876e3055f
Jason Steffener <js2746@columbia.edu>
2012-11-29 08:19:15 -0500

    minor changes

--- FinalCode/subfnVoxelWiseProcessBatch.m
+++ FinalCode/subfnVoxelWiseProcessBatch.m
@@ -82,10 +82,10 @@ for i = 1:Nvoxels
             end
 
         case '4'
-            temp.X = data.X(:,:,i);
+            temp.X = data.X;
             temp.M = data.M(:,:,i);
-            temp.Y = data.Y(:,:,i);
-            temp.COV = data.COV(:,:,i);
+            temp.Y = data.Y;
+            temp.COV = data.COV;
             if sum(isnan(temp.M)) == 0; 
                 AllDataFlag = 1;
             end
@@ -123,8 +123,9 @@ for i = 1:Nvoxels
     end
     
     if AllDataFlag
+        temp.Nboot = Nboot;
         Parameters = subfnProcess(temp);
-        Parameters{i}.Nboot = data.Nboot;
+        Parameters{i}.Nboot = Nboot;
         Parameters{i}.Thresholds = data.Thresholds;
     end
 
