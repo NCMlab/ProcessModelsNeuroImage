407a61d36b18081772cd07f2dfa6df6b9e20f339
steffejr <steffener@gmail.com>
2014-02-07 07:40:15 -0500

    working on a generic path model with interactions estimator

---
+++ WIPCode/WIPestimateAnyModel.m
@@ -0,0 +1,119 @@
+clear
+N = 100;
+M = 3;
+Data = randn(N,M);
+% Create the direct effects model
+Direct = zeros(M,M);
+Direct([1],2) = 1;
+Direct([1 2],3) = 1;
+
+% Interactions
+Inter = zeros(M,M);
+%Inter([4 5],3,1) = 1;
+%Inter([1 2 3],2,1) = 1;
+
+% Estimate the paths
+Paths = zeros(M,M,1);
+Paths(1,2,1) = 1;
+Paths(2,3,1) = 2;
+
+Direct
+Inter
+Paths
+%%
+MaxNumberInter = 0;
+if ~isempty(find(Inter))
+    MaxNumberInter = size(Inter,3);
+end
+MaxNumberInter
+% Make sure the interaction matrix contains all the main effects
+%if isempty(find(Inter ~= Direct.*Inter))
+% The interaction terms are good
+%end
+% Fit the regression equations for direct effects
+beta = zeros(M+1+MaxNumberInter,M);
+for i = 1:M
+    Col = find(Direct(:,i));
+    if ~isempty(Col)
+        % Check for interaction terms for this dependent variable
+        Interaction = [];
+        if ~isempty(find(Inter(:,i,:)))
+            Interaction = zeros(N,MaxNumberInter);
+            
+            for j = 1:MaxNumberInter
+                InterCol = find(Inter(:,i,j));
+                Interaction(:,j) = prod(Data(:,InterCol),2);
+            end
+        end
+        b = subfnregress(Data(:,i),[Data(:,Col) Interaction]);
+        beta([1; 1+find(Direct(:,i))],i) = b(1:length(Col)+1);
+        if ~isempty(Interaction)
+            % Add the interaction terms
+            beta(M+2:end,i) = b(length(Col)+2:end);
+        end
+    end
+end
+beta
+
+
+NumberOfPaths = size(Paths,3);
+IndirectEffect = cell(size(Paths,3),1);
+% add a row of zeros so that the indexing matchs with the beta matrix
+Paths = [zeros(1,M,NumberOfPaths); Paths];
+for j = 1:NumberOfPaths
+    % are there any interactions along this path?
+    if isempty(find(Paths(2:end,:,j).*Inter))
+        % no interactions
+        IndirectEffect{j} = 1;
+        for i = 1:max(max(Paths(:,:,j)))
+            index = find(Paths(:,:,j)==i);
+            [Row Col] = ind2sub([M+1,M],index);
+            IndirectEffect{j} = IndirectEffect{j}.*beta(index);
+        end
+    else
+        IndirectEffect{j} = ones(10,1);
+        for i = 1:max(max(Paths(:,:,j)))
+            index = find(Paths(:,:,j)==i);
+            [Row Col] = ind2sub([M+1,M],index);
+            % is there an interaction term for this column?
+            % Only probe an interaction if there is an interaction at some
+            % point in the path
+            if MaxNumberInter
+                for k = 1:MaxNumberInter
+                    if ~isempty(Inter(Row,Col,k))
+                        % find the OTHER terms that interact with the path of
+                        % interest
+                        tempInter = Inter(:,Col,k);
+                        tempInter(Row) = 0;
+                        % find the probe values,this even works for higher
+                        % order interactions, I think
+                        IndirectEffect{j} = IndirectEffect{j}.*beta(index) + beta(M+1+k,Col).*[0; prod(prctile(Data(:,find(tempInter)),[10:10:90]),2)];
+                    end
+                end
+            
+            end
+            
+        end
+    end
+end
+IndirectEffect
+%%
+data = []
+data.names.X = 'X';
+data.names.M{1} = 'M';
+data.names.Y = 'Y';
+data.names.V='';
+data.names.W='';
+data.X = Data(:,1);
+data.M = Data(:,2);
+data.Y = Data(:,3);
+data.COV = [];
+data.V = [];
+data.ModelNum = '4'
+PointEstFlag = 1;
+[ParameterToBS Parameters] = subfnProcessModelFit(data,PointEstFlag)
+
+
+Parameters.Model1{1}
+
+
